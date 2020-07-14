767c8b9610d722c6917db9e4330a601816c4808e
NickV056 <nickvillanueva069@gmail.com>
2020-07-14 18:04:43 -0400

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,63 @@
+
+
+player1 = input("Player1? ")
+player2 = input("Player2? ")
+
+print("Player 1 wins") 
+
+player2 == "rock"
+print("Tie")
+I stated the outcomes above
+Rock and Rock
+Rock and Paper
+Rock and Scissors
+
+Scissors and Rock
+Scissors and Scissors
+Scissors and Paper
+Paper and Rock
+Paper and Scissors
+Paper and Paper
+
+if (player1 == "rock" and player2 == "scissors"):
+    print("Player 1 wins")
+if (player1 == "rock" and player2 == "paper"):
+    print("Player 2 wins")
+if (player1 == "rock" and player2 == "rock"):
+    print("Tie")
+if (player1 == "scissors" and player2 == "scissors"):
+    print("Tie")
+if (player1 == "scissors" and player2 == "rock"):
+    print("Player 2 wins")
+if (player1 == "scissors" and player2 == "paper"):
+    print("Player 1 wins")
+if (player1 == "paper" and player2 == "scissors"):
+    print("Player 2 wins")
+if (player1 == "paper" and player2 == "rock"):
+    print("Player 1 wins")
+if (player1 == "paper" and player2 == "paper"):
+    print("Tie")    
+
+if player1 == player2:
+    print("Tie")
+elif player1 == 'rock' and player2 == 'scissors':
+    print("Player 1 wins.")
+elif player1 == 'paper' and player2 == 'rock':
+    print("Player 1 wins.")
+elif player1 == 'scissors' and player2 == 'paper':
+    print("Player 1 wins.")
+elif player2 == 'rock' and player1 == 'scissors':
+    print("Player 2 wins.")
+elif player2 == 'paper' and player1 == 'rock':
+    print("Player 2 wins.")
+elif player2 == 'scissors' and player1 == 'paper':
+    print("Player 2 wins.")
+
+
+
+
+
+
+
+
+
